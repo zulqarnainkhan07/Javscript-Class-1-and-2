5e7a830a41727374ac6a36eb250734b94688e6d2
zulqarnainkhan07 <khanzulqarnain752@gmail.com>
2023-08-31 22:27:42 +0500

    Js Assignment

---
+++ Chp 1 (Alert)/app.js
@@ -0,0 +1,21 @@
+// Name : Zulqarnain Ahmed 
+// Roll : WMA-130133                       
+                       //------------Chapter no 1-----------//
+
+// Q1 Answer//
+alert ("Hello Welcome");
+
+// Q2 Answer //
+alert("Error! Please enter a valid password.!");
+
+// Q3 Answer //
+alert("Welcome to JS land..! \nHappy coding!");
+
+// Q4 Answer //
+alert ("Welcome to JS land....");
+alert ("Happy coding.!\n ▢ prevent this page from creating additional dialog");
+
+// Q5 Answer //
+let text1;
+let text2= prompt("Please enter your Message :");
+alert (text2);
---
+++ Chp 2 (Variable,String)/app.js
@@ -0,0 +1,44 @@
+// Name : Zulqarnain Ahmed 
+// Roll : WMA-130133                       
+              
+              //------------Chapter no 2-----------//
+
+// Q 1 Answer //
+var username;
+
+// Q 2 Answer //
+var myName = "Zulqarnain Ahmed";
+console.log (myName);
+
+// Q 3 Answer //
+var message = "Hello World";
+alert (message);
+
+// Q 4 Answer //
+ var name="Hello There ...!\nMy Name is Zulqarnain Ahmed";
+ var age= "I'm 19 Years old";
+ var aim= "My aim is to become certified Website and Mobile/ios App developer";
+
+ alert (name);
+ alert (age);
+ alert (aim);
+
+ // Q 5 Answer //
+ var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+ alert(pizza);
+
+ // Q 6 Answer //
+ var email="khanzulqarnain752@gmail.com";
+ var text ="My Email adress is : ";
+ alert (text + email);
+
+// Q 7 Answer //
+var book ="I am trying to learn from the book A smarter way to learn JavaScript";
+alert (book);
+
+// Q 8 Answer //
+document.write("<h1>Yah! I Can write HTML content through JavaScript</h1>");
+
+// Q 9 Answer //
+var stylishLine="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(stylishLine);
